11df22c6cf4a9bf599f2ca01ba0baf57b3036997
Rohr, Fabian <fabian.rohr@maibornwolff.de>
2021-02-04 14:26:44 +0100

    goo

--- src/store/model/amazon-de.ts
+++ src/store/model/amazon-de.ts
@@ -1,78 +1,78 @@
 import {Store} from './store';
 
 export const AmazonDe: Store = {
-	backoffStatusCodes: [403, 429, 503],
-	currency: '€',
-	labels: {
-		captcha: {
-			container: 'body',
-			text: [
-				'geben sie die unten angezeigten zeichen ein',
-				'geben sie die zeichen unten ein'
-			]
-		},
-		inStock: {
-			container: '#merchant-info',
-			text: ['Verkauf und Versand durch Amazon.']
-		},
-		maxPrice: {
-			container: '#priceblock_ourprice',
-			euroFormat: true
-		},
-		outOfStock: [
-			{
-				container: '#availability',
-				text: ['Derzeit nicht verfügbar']
-			}
-		]
-	},
-	links: [
-		{
-			brand: 'sony',
-			cartUrl:
-				'https://www.amazon.de/gp/aws/cart/add.html?ASIN.1=B08H93ZRK9&Quantity.1=1&tag=o5-21',
-			labels: {
-				inStock: {
-					container: '#productTitle',
-					text: ['sony playstation 5']
-				}
-			},
-			model: 'ps5 console',
-			series: 'sonyps5c',
-			url: 'https://www.amazon.de/dp/B08H93ZRK9?tag=o5-21'
-		},
-		{
-			brand: 'sony',
-			cartUrl:
-				'https://www.amazon.de/gp/aws/cart/add.html?ASIN.1=B08H98GVK8&Quantity.1=1&tag=o5-21',
-			labels: {
-				inStock: {
-					container: '#productTitle',
-					text: ['sony playstation 5']
-				}
-			},
-			model: 'ps5 digital',
-			series: 'sonyps5de',
-			url: 'https://www.amazon.de/dp/B08H98GVK8?tag=o5-21'
-		},
-		{
-			brand: 'microsoft',
-			labels: {
-				inStock: {
-					container: '#productTitle',
-					text: ['xbox series x']
-				}
-			},
-			model: 'xbox series x',
-			series: 'xboxsx',
-			url: 'https://amzn.to/37dIUIQ'
-		},
-		{
-			brand: 'test:brand',
-			model: 'test:model',
-			series: 'test:series',
-			url: 'https://amzn.to/3nV9DiC'
-		},
-	],
-	name: 'amazon-de'
+  backoffStatusCodes: [403, 429, 503],
+  currency: '€',
+  labels: {
+    captcha: {
+      container: 'body',
+      text: [
+        'geben sie die unten angezeigten zeichen ein',
+        'geben sie die zeichen unten ein'
+      ]
+    },
+    inStock: {
+      container: '#merchant-info',
+      text: ['Verkauf und Versand durch Amazon.']
+    },
+    maxPrice: {
+      container: '#priceblock_ourprice',
+      euroFormat: true
+    },
+    outOfStock: [
+      {
+        container: '#availability',
+        text: ['Derzeit nicht verfügbar']
+      }
+    ]
+  },
+  links: [
+    {
+      brand: 'sony',
+      cartUrl:
+        'https://www.amazon.de/gp/aws/cart/add.html?ASIN.1=B08H93ZRK9&Quantity.1=1&tag=o5-21',
+      labels: {
+        inStock: {
+          container: '#productTitle',
+          text: ['sony playstation 5']
+        }
+      },
+      model: 'ps5 console',
+      series: 'sonyps5c',
+      url: 'https://www.amazon.de/dp/B08H93ZRK9?tag=o5-21'
+    },
+    {
+      brand: 'sony',
+      cartUrl:
+        'https://www.amazon.de/gp/aws/cart/add.html?ASIN.1=B08H98GVK8&Quantity.1=1&tag=o5-21',
+      labels: {
+        inStock: {
+          container: '#productTitle',
+          text: ['sony playstation 5']
+        }
+      },
+      model: 'ps5 digital',
+      series: 'sonyps5de',
+      url: 'https://www.amazon.de/dp/B08H98GVK8?tag=o5-21'
+    },
+    {
+      brand: 'microsoft',
+      labels: {
+        inStock: {
+          container: '#productTitle',
+          text: ['xbox series x']
+        }
+      },
+      model: 'xbox series x',
+      series: 'xboxsx',
+      url: 'https://amzn.to/37dIUIQ'
+    },
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url: 'https://amzn.to/3nV9DiC'
+    },
+  ],
+  name: 'amazon-de'
 };
